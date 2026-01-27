3d41c8c75e8482c555fe5cc0031c7ef46dbdc88b
niklasmarxer <niklas.marxer@tum.de>
2026-01-26 20:08:29 -0800

    Refactor: Remove legacy ThermodynamicProperties.py

--- steer_core/Constants/__init__.py
+++ steer_core/Constants/__init__.py
@@ -2,10 +2,6 @@
 
 This facade keeps downstream imports short during development
 by exposing all symbols from the core constant submodules.
-
-NOTE: ThermodynamicProperties (CO2, Water, MEA, etc.) have been moved to:
-  - steer-materials: CO2, N2, FlueGas, Water, Steam, NaturalGas
-  - steer_ccus_tea: MEA and VLE functions
 """
 
 from .Units import *  # noqa: F401,F403
