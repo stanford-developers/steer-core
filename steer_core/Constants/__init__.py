7ffeabfda8b0416d954bbb9deae0704e33eeb92c
niklasmarxer <niklas.marxer@tum.de>
2026-01-28 15:16:21 -0800

    init update

--- steer_core/Constants/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Convenience exports for steer_core.Constants.
-
-This facade keeps downstream imports short during development
-by exposing all symbols from the core constant submodules.
-"""
-
-from .Units import *  # noqa: F401,F403
-from .Universal import *  # noqa: F401,F403
