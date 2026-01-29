3c5c650ea8a9ec8a214d5f258f6b550f049dc9fe
fschweden <148897399+fschweden@users.noreply.github.com>
2026-01-29 09:55:54 -0800

    added Molar Masses

--- steer_core/Constants/Universal.py
+++ steer_core/Constants/Universal.py
@@ -41,6 +41,16 @@ GPU_TO_SI = 3.35e-10  # mol/(m²·s·Pa) per GPU (gas permeation unit)
 # =============================================================================
 # MOLAR MASSES
 # =============================================================================
-M_G_PER_MOL_NO2 = 46.0055
-M_G_PER_MOL_SO2 = 64.0638
-MW_G_PER_MOL_CO2 = 44.0095
+G_PER_MOL_NO2 = 46.0055
+G_PER_MOL_SO2 = 64.0638
+G_PER_MOL_N2    = 28.0134
+G_PER_MOL_O2    = 31.9988
+G_PER_MOL_H2O   = 18.0152
+G_PER_MOL_CO2   = 44.0095
+G_PER_MOL_AR    = 39.9480
+G_PER_MOL_H2    = 2.0158
+G_PER_MOL_CH4   = 16.0425
+G_PER_MOL_C2H6  = 30.0690
+G_PER_MOL_C3H8  = 44.0956
+G_PER_MOL_CO    = 28.0101
+G_PER_MOL_C4H10 = 58.1222
